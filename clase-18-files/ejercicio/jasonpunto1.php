816fc9b5af6736a69ba93121fe8dadbed15fd2bc
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-08-16 12:36:32 -0300

    nueva clase

---
+++ clase-18-files/ejercicio/jasonpunto1.php
@@ -0,0 +1,14 @@
+<?php
+
+// $a = ['a' => 1, 'b' => 2, 'c' => 'Yo <3 JSON'];
+
+// echo $a;
+
+$a = ['a' => 1, 'b' => 2, 'c' => 'Yo <3 JSON'];
+echo json_encode($a);
+echo "<hr>";
+
+$b = json_encode($a);
+echo var_dump(json_decode($b,true));
+echo "<hr>";
+
---
+++ clase-18-files/ejercicio/pepe.php
@@ -0,0 +1,8 @@
+<?php
+$request = "https://dev.digitalhouse.com/api/getcomisiones";
+$get_comisiones = file_get_contents($request);
+var_dump($get_comisiones);
+$get_comisiones_array = json_decode($get_comisiones, true);
+echo "<pre>";
+var_dump($get_comisiones_array);
+echo "</pre>";
